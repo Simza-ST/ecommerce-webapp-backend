5486c178ea91d07b51f802decb6b6f6d97ca2d9b
Simza-ST <simzast123@gmail.com>
2025-06-22 12:05:46 +0200

    initial commit

---
+++ src/main/java/com/simzaconnections/ecommerce_webapp_backend/EcommerceWebappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simzaconnections.ecommerce_webapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simzaconnections/ecommerce_webapp_backend/EcommerceWebappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simzaconnections.ecommerce_webapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
